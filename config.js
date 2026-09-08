0ecc8f48f28756a053afdf6f82c123bf83cdf116
srikanthkoniki <srikanth.koniki@gmail.com>
2026-09-09 00:50:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.PLANNER_CONFIG = {
-  CLIENT_ID: "srikanthkoniki@gmail.com",
+  CLIENT_ID: "899516323937-95k8lb08l12atedp8mf6e8uvenjkjm1g.apps.googleusercontent.com",
   SHEET_NAME: "Srikanth Personal Planner"
 };
